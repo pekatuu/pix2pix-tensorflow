e6cc871f10fe6149b1cb8aa17ccf52e510982d51
pekatuu <pekatuu@gmail.com>
2016-12-14 20:01:26 +0900

    add interp option to specify number of B

--- convert_mot.py
+++ convert_mot.py
@@ -8,9 +8,10 @@ import scipy.misc
 def parse_args():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('--mot_root', required=True)
-    parser.add_argument('--img_size', default=128)
+    parser.add_argument('--img_size', default=128, type=int)
     parser.add_argument('--output_dir', required=True)
-    parser.add_argument('--step', default=5)
+    parser.add_argument('--interp_nr', default=1, type=int)
+    parser.add_argument('--step', default=5, type=int)
     return parser.parse_args()
 
 
@@ -31,20 +32,21 @@ def gen_out_name(output_dir, start=1):
         i += 1
 
 
-def convert_dir(img_dir, out_name_generator, img_size, step):
+def convert_dir(img_dir, out_name_generator, img_size, interp_nr, step):
     g = gen_image_path(img_dir, step)
     try:
+        a1 = g.next()
         while True:
-            out_image = np.empty((img_size, img_size * 3, 3), dtype=np.uint8)
-            a1 = g.next()
-            b = g.next()
+            out_image = np.empty((img_size, img_size * (interp_nr + 2), 3), dtype=np.uint8)
+            bz = [g.next() for x in range(interp_nr)]
             a2 = g.next()
-            for i, path in enumerate([b, a1, a2]):
+            for i, path in enumerate(bz + [a1, a2]):
                 im = scipy.misc.imresize(scipy.misc.imread(path), (img_size, img_size))
                 out_image[:, img_size * i:img_size * (i + 1), :] = im
             out_name = out_name_generator.next()
             scipy.misc.imsave(out_name, out_image)
-            print("join {} {} {} into {}".format(a1, a2, b, out_name))
+            print("join {} {} {} into {}".format(a1, a2, bz, out_name))
+            a1 = a2
     except StopIteration:
         pass
 
@@ -71,7 +73,7 @@ def convert_dirs(args, dir_name, dirs):
     mkdirp(out_dir_train)
     train_name_generator = gen_out_name(out_dir_train)
     for train_dir in dirs:
-        convert_dir(train_dir, train_name_generator, args.img_size, args.step)
+        convert_dir(train_dir, train_name_generator, args.img_size, args.interp_nr, args.step)
 
 
 if __name__ == '__main__':
